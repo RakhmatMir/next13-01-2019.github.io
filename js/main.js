277bc1b95dbacd30c7fd10e341ddeb2c477a37ad
Rakhmat <tavozmrr@gmail.com>
2019-01-13 17:42:38 +0500

    next13,01,2019

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementsByClassName("left-block")[0].style.width = "250" + 'px';
+    }
+    function closeNav() {
+    document.getElementsByClassName("left-block")[0].style.width = "0";
+    }
